ee525f5e69716c4dd63bc9bc58a98c28ee82912c
Robert Hartnett <rhartnett35@gmail.com>
2017-10-03 01:48:05 -0400

    more learning

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import sqlite3 as sql
+import flask
+from flask import abort, redirect, url_for
+from flask import render_template
 from flask import Flask
 easi = Flask(__name__)
 
 @easi.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 @easi.route('/api')
 def api():
-    return 'Api Goes Here.'
+    return redirect(url_for('index'))
+
+@easi.errorhandler(404)
+def notfound(error):
+    return 'This is not the page you are looking for.'
